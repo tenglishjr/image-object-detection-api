7cc99bd9f5d1445eee181010440b81a11cad6de8
TJ English <teej.english@gmail.com>
2024-08-22 10:28:28 -0500

    Project creation

---
+++ src/main/java/org/example/imageobjectdetectionapi/ImageObjectDetectionApi.java
@@ -0,0 +1,13 @@
+package org.example.imageobjectdetectionapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImageObjectDetectionApi {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImageObjectDetectionApi.class, args);
+	}
+
+}
